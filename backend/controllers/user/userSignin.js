9d75885d582332ea879321c0b9128bce6225aac1
manikanta <a.manikanta9810@gmail.com>
2025-09-04 22:44:09 +0530

    same site login

--- backend/controllers/user/userSignin.js
+++ backend/controllers/user/userSignin.js
@@ -29,7 +29,8 @@ async function userSignInController(req, res){
             const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, {expiresIn :60 *60 * 8});
             const tokenOption = {
                 httpOnly : true,
-                secure: true
+                secure: true,
+                sameSite:'None'
             }
             res.cookie("token", token,tokenOption).status(200).json({
                 message: "login successfully",
